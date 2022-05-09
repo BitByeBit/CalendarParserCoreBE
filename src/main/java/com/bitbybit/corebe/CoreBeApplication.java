b48686adc9dfb803ab417009bf87a3a735e3617f
Tudor Pescaru <tudorp00@gmail.com>
2022-05-09 23:23:52 +0300

    BIT-11 adds initial project structure

---
+++ src/main/java/com/bitbybit/corebe/CoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.bitbybit.corebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitbybit/corebe/CoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbybit.corebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
