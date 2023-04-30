46c92af97bd26c2132e448a4ab11ca581c352dc7
Matei Iordache <miordache@bitdefender.com>
2023-04-30 22:55:25 +0300

    Add prometheus basic config in java

--- src/main/java/com/bitbybit/corebe/services/AuthService.java
+++ src/main/java/com/bitbybit/corebe/services/AuthService.java
@@ -4,17 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import io.prometheus.client.Counter;
 
 @Service
 public class AuthService {
     @Value("${authenticator.url}")
     private String authenticatorUrl;
 
+    static final Counter requests = Counter.build()
+     .name("valid_tokens").help("valid_tokens").register();
+
     public boolean isInvalidUser(String userUid, String token) {
         return !userUid.equals(validateToken(token.split(" ")[1].trim()));
     }
 
     public String validateToken(String token) {
+        requests.inc();
         WebClient webClient = WebClient.create(authenticatorUrl);
         return webClient.post()
                 .uri("/validate")
